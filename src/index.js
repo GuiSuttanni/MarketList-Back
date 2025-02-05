3bec9666b64aeed4a36e424936f0d4215a1aa427
Guilherme Suttanni <guilherme.ferreira@mv.com.br>
2025-02-05 14:53:54 -0300

    Restore

--- src/index.js
+++ src/index.js
@@ -8,6 +8,5 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/shopping-list', shoppingListRoutes);
 
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
