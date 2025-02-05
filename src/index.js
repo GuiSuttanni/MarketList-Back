6a3e96b316fcb8430dc666cb5184a744015af8ad
Guilherme Suttanni <guilherme.ferreira@mv.com.br>
2025-02-05 14:48:57 -0300

    Tentativa de deploy

--- src/index.js
+++ src/index.js
@@ -8,5 +8,6 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/shopping-list', shoppingListRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+module.exports = app;
